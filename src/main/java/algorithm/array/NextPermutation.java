8d954c0405f0d89db2daea4998cbaef1b798a8c9
maxthinkthink <maxthinkthink@aliyun.com>
2023-12-17 15:41:48 +0800

    next pemutation

---
+++ src/main/java/algorithm/array/NextPermutation.java
@@ -0,0 +1,37 @@
+package algorithm.array;
+
+import java.util.Arrays;
+
+public class NextPermutation {
+    public static void nextPermutation(int[] nums) {
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i-1] < nums[i]) {
+                // （i， n）范围内 比 i-1 大的最小值
+                int pre = nums[i];
+                int preI = i;
+                for (int j = i; j <= nums.length -1; j++) {
+                    if (nums[j] > nums[i-1] && nums[j] < pre) {
+                        pre = nums[j];
+                        preI = j;
+                    }
+                }
+                int tmp = nums[preI];
+                nums[preI] = nums[i-1];
+                nums[i-1] = tmp;
+
+                // i,n 范围内的数据 从小打大排序
+                Arrays.sort(nums, i, nums.length);
+                return;
+            }
+        }
+
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int tmp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+}
---
+++ src/main/java/algorithm/str/Partition.java
@@ -0,0 +1,4 @@
+package algorithm.str;
+
+public class Partition {
+}

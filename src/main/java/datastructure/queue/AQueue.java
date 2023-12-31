14346f83aa629601ef88e3f9ad9ae03aaf0c0328
maxthinkthink <maxthinkthink@aliyun.com>
2023-12-31 14:00:56 +0800

    fix

---
+++ src/main/java/datastructure/queue/AQueue.java
@@ -0,0 +1,14 @@
+package datastructure.queue;
+
+import java.util.*;
+
+public class AQueue {
+    public static void main(String[] args) {
+        Queue<String> queue = new ArrayDeque<>();
+        Deque<String> deque = new LinkedList<>();
+        PriorityQueue<String> priorityQueue = new PriorityQueue<String>();
+        Stack<String> stack = new Stack<>();
+        Set<String> set = new HashSet<>();
+
+    }
+}

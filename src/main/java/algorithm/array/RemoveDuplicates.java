a5110552b8930f634daa8ef91ec3f926aa0dd2a9
xiangxiangzhou <xiangxiangzhou@aliyun.com>
2024-03-23 11:23:06 +0800

    remote duplicates

--- src/main/java/algorithm/array/RemoveDuplicates.java
+++ src/main/java/algorithm/array/RemoveDuplicates.java
@@ -4,26 +4,24 @@ import java.util.Arrays;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int[] a = new int[] {1,2};
+        int[] a = new int[] {1,1,1,2,2,3};
         int l = removeDuplicates(a);
         System.out.println(l);
         Arrays.stream(a).mapToObj(x -> x  + " ").forEach(System.out::print);
     }
 
     public static int removeDuplicates(int[] nums) {
-        int s = 0;
-        int f = 0;
-        for (; f < nums.length; f++) {
-            if (nums[s] != nums[f]) {
-                nums[++s] = nums[f];
-            } else {
-                if (s >= 1 && nums[s-1] == nums[s] ) {
-                    continue;
-                } else {
-                    nums[++s] = nums[f];
-                }
-            }
+        if (nums.length <= 2) {
+            return nums.length;
         }
-        return s + 1;
+        int s = 2;
+        int f = 2;
+        while (f < nums.length) {
+            if (nums[s-2] != nums[f]) {
+                nums[s++] = nums[f];
+            }
+            f++;
+        }
+        return s;
     }
 }
